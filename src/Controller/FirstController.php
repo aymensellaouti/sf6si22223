8ce58c8ed179561e964226664dd8b4394de08485
aymen2782 <aymen.sellaouti@gmail.com>
2022-09-20 09:46:36 +0100

    first controller

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'app_first')]
+    public function index(): Response
+    {
+        $x = 'Bonjour SI2';
+        dump($x);
+        return $this->render('first/index.html.twig', [
+            'name' => 'FirstController',
+            'firstname' => 'aymen'
+        ]);
+    }
+
+    #[Route('/hello/{name}', name: 'hello')]
+    public function sayHello($name): Response
+    {
+        return $this->render('first/hello.html.twig', [
+            'name' => $name,
+        ]);
+    }
+}
